071c7938cffbb1312e424b6283ede4ce4d145467
gabriel0067 <gabrielrdp98@gmail.com>
2024-08-23 22:28:24 -0300

    commit

---
+++ aula 02/aula02.sql
@@ -0,0 +1,27 @@
+-- Criar banco de dados bd_sistema_bancario
+
+CREATE DATABASE bd_sistema_bancario;
+
+-- Criar 5 tabelas quaisquer
+
+CREATE TABLE nome(id int, nomes varchar);
+CREATE TABLE telefone(numero int, ddd int);
+CREATE TABLE cidade(estado varchar, pais varchar);
+CREATE TABLE email(ende varchar);
+CREATE TABLE cep(id int);
+
+-- Criar tabelas conforme MER (Sistema Bancario):
+
+
+
+-- Deletar as 5 tabelas criadas aleatoriamente
+
+
+
+-- alterar as tabelas conforme o MER atualizado
+
+
+
+-- Criar um esquema com o seu nome
+
+
--- aula 05/aula05.sql
+++ aula 05/aula05.sql
@@ -46,14 +46,40 @@ FULL JOIN tbl_livros l ON t.codigo_titulo = l.codigo_titulo;
 
 --2 Liste os títulos e suas descrições dos livros alugados (retorna 4 linhas)
 
-
+SELECT t.titulo, t.descricao
+FROM tbl_livros l
+JOIN tbl_titulo t ON l.codigo_titulo = t.codigo_titulo
+WHERE l.status = 'ALUGADO';
 
 --3 Liste os nomes dos clientes que não têm livros alugados (retorna 1 linha)
 
+SELECT c.nome
+FROM tbl_cliente c
+LEFT JOIN tbl_emprestimo e ON c.codigo_cliente = e.codigo_cliente
+WHERE e.codigo_cliente IS NULL;
+
+
 --4 Liste os títulos e suas categorias dos livros disponiveis (retorna 1 linha).
+
+SELECT t.titulo, t.categoria
+FROM tbl_livros l
+JOIN tbl_titulo t ON l.codigo_titulo = t.codigo_titulo
+WHERE l.status = 'DISPONIVEL';
 
 
 --5 Liste os nomes dos clientes e os títulos dos livros que eles têm alugados (retorna 5 linhas).
 
---6 Retorne o nome, titulo do livro e o status do esmprestimo do livro alugado pela Ana Oliveira (retorna 1 linha)
+SELECT c.nome, t.titulo
+FROM tbl_cliente c
+LEFT JOIN tbl_emprestimo e ON c.codigo_cliente = e.codigo_cliente
+LEFT JOIN tbl_livros l ON e.codigo_livro = l.cod_livro
+LEFT JOIN tbl_titulo t ON l.codigo_titulo = t.codigo_titulo;
 
+--6 Retorne o nome, titulo do livro e o status do esmprestimo do livro alugado pela Ana Oliveira (retorna 1 linha)
+ 
+SELECT c.nome, t.titulo, l.status
+FROM tbl_cliente c
+JOIN tbl_emprestimo e ON c.codigo_cliente = e.codigo_cliente
+JOIN tbl_livros l ON e.codigo_livro = l.cod_livro
+JOIN tbl_titulo t ON l.codigo_titulo = t.codigo_titulo
+WHERE c.nome = 'Ana Oliveira';
